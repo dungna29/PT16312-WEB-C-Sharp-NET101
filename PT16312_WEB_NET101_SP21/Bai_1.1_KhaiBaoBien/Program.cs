27f163a35d06b71a7b7013a053ac50ab417481aa
Dung Nguyen Anh (FE FPL HN) <DungNA29@fe.edu.vn>
2021-01-09 11:18:48 +0700

    Dungna29

---
+++ PT16312_WEB_NET101_SP21/Bai_1.1_KhaiBaoBien/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_1._1_KhaiBaoBien
+{
+    class Program
+    {
+        /*
+         * Bài 1.1 Khai báo biến
+         * Định nghĩa biến: Dùng để lưu trữ giá trị hoặc 1 tập giá trị và biến
+         * được khở tạo trong bộ nhớ ảo khi chương trình khởi chạy.
+         *
+         * Công thức: <Kiểu dữ liệu> <Tên Biến> = <Giá trị>;
+         * 1. <Kiểu dữ liệu>
+         *  + Số nguyên:sbyte, byte, long, int
+         *  + Sô thực: float, double, decimal
+         *  + Chuỗi hoặc ký tự: string, char
+         *  + Logic: bool
+         *  + var: nó có thể đại diện là tất cả các kiểu dữ liệu.
+         * 2. <Tên Biến>
+         *  + Quy tắc:
+         *      - Tên biến phải có nghĩa
+         *      - Có phân biệt chữ hoa chữ thường
+         *      - Không bắt đầu bởi số, không dùng từ khóa
+         *      - Cố gắng làm quen đặt tên biến với Tiếng Anh
+         *  + Khai báo tên biến với 2 từ trở lên:
+         *      - Cách 1: normal = firstname
+         *      - Cách 2: CamelCase = FirstName, firstName
+         *      - Cách 3: Under_Score = first_name, first_Name
+         *  + Biến cục bộ: là biến được khai báo bên trong 1 hàm.
+         *  + Biến toàn cục: là biến được khai báo bên ngoài hàm.
+         *      - Khi khai báo biến toàn cục sử dụng dấu gạch dưới _ trước tên biến.
+         *      - int _year = 1989;
+         * 3.<Giá trị>
+         *  + Khi khai báo biến có thể khởi tạo giá trị hoặc không khởi tạo giá trị.
+         *  + Giá trị phải đúng các quy tắc với kiểu dữ liệu
+         *
+         */
+        static void Main(string[] args)
+        {
+            //Khai báo chuỗi:
+            string name = "Dungna29";
+
+            //Khai báo kiểu var;
+            var name1 = "Dungna28";
+            var year = 2021;
+            var diem = 8.6;
+        }
+    }
+}
---
+++ PT16312_WEB_NET101_SP21/Bai_1.2_NhapXuatTuBanPhim/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_1._2_NhapXuatTuBanPhim
+{
+    class Program
+    {
+        /*
+         * BÀI 1.2 Nhập xuất dữ liệu từ bàn phím
+         *  1. Xuất dữ liệu ra màn hình
+         *      + Phím tắt : cw + tab
+         *      + Console.WriteLine(); Ghi xong thì xuống dòng
+         *      + Console.Write(); Ghi xong thì không xuống dòng
+         *  2. Nhập dữ liệu từ bàn phím:
+         *      +Console.Read(); Đọc 1 ký tự từ bàn phím
+         *      +Console.ReadLine();  Đọc nhiều ký tự từ bàn phím
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Chao cac ban den voi C#1");
+            Console.Write(" 2021 ");
+            Console.WriteLine(" Chao cac ban den voi C#1");
+            Console.ReadKey();
+        }
+    }
+}
